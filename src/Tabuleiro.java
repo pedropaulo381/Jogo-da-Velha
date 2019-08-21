f1b777241351ce440a8fd0db8293c6c0a0212ed0
Pedro Paulo <pedropauloppkk123@gmail.com>
2019-08-21 16:23:20 -0300

    Tela finalizada e tabuleiro criado

--- src/Tabuleiro.java
+++ src/Tabuleiro.java
@@ -1,4 +1,14 @@
 
 public class Tabuleiro {
-    
+    private Sting tabuleiro[][] = new String[3][3];
+    public void adicionarJogada(int x, int y, String valor){
+        this.tabuleiro[x][y] = valor;
+    }
+    public void limparTabuleiro(){
+        for(int = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                tabuleiro[i][j] = " ";
+            }
+        }
+    }
 }
--- src/Tela.java
+++ src/Tela.java
@@ -1,9 +1,56 @@
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JFrame;
-public class Tela extends JFrame{
+import javax.swing.JButton;
+
+public class Tela extends JFrame implements ActionListener{
+
+    JButton[][] botoes = new JButton[3][3];
+    int jogador = 1;
+    
+
+
+
     public Tela(){
         super("Jogo da Velha - Tec. Desenv. de jogos");
         this.setVisible(true);
+        this.setLayout(new GridLayout(3, 3));
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setExtendedState(MAXIMIZED_BOTH);
+        
+        for(int i=0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                botoes[i][j] = new JButton();
+                botoes[i][j].addActionListener(this);
+                this.add(botoes[i][j]);
+            }
+        }
+       
     }
+    
+       @Override
+         public void actionPerformed(ActionEvent e){
+        for(int i = 0; i< 3; i++){
+            for (int j = 0; j < 3; j++){
+                if(e.getSource() == botoes[i][j]){
+                   jogar(i, j, jogador);
+                }
+            }
+        }
+         }
+      public void jogar(int x, int y, int jogador){
+          
+          String texto;
+          
+          if(jogador == 1){
+              texto = "X";
+              this.jogador = 2;
+          }else{
+              texto = "O";
+              this.jogador = 1;
+          }
+           botoes[x][y].setText(texto);
+      }
+     
 }
